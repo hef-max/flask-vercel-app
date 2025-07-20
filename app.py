14473e6f19e1bb727d93f7d1e68c21d7b9d3c767
Hefri Anesti Purnama <hefrykun10@gmail.com>
2025-07-20 19:24:28 +0700

    updates files

--- app.py
+++ app.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route('/api/data/terms_of_service')
+def get_terms_of_service():
+    return render_template('terms_of_service.html')
+
+@app.route('/api/data/privacy_policy')
+def get_privacy_policy():
+    return render_template('privacy_policy.html')
+
+@app.route('/api/data/community_guidelines')
+def get_community_guidelines():
+    return render_template('community_guidelines.html')
